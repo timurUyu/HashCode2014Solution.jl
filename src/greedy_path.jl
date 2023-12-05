63c0e366e2bfbe90281f4c898e64aa232a0b80c9
Adrian Anaemeje <anaemeje@mit.edu>
2023-12-05 18:02:25 -0500

    Add greedy_path function

---
+++ src/greedy_path.jl
@@ -0,0 +1,38 @@
+using HashCode2014;
+using StaticArrays;
+using Printf;
+
+
+"""
+    greedy_path(rg::RouteGrid, starting_junction::Int, total_duration::Int; n::Int=1)
+
+Find a path through the city by always choosing the best neighbor to visit next. The best
+neighbor is the one that leads to the longest path. Path length is `n=1` by default, but can
+be changed.
+
+# Arguments
+- `rg::RouteGrid`: a city, in the form of a RouteGrid
+- `starting_junction::Int`: the junction from which to start
+- `total_duration::Int`: the total duration of the path
+- `n::Int=1`: the length of the path to consider when choosing the best neighbor
+"""
+function greedy_path(rg::RouteGrid, starting_junction::Int, total_duration::Int; n::Int=1)
+    t::Int = 0
+
+    # Initialize a path to build upon
+    path = zeros(Int, length(rg.neighbors))
+    path[1] = starting_junction
+    current_idx = 1
+    best_neighbor = RouteGridNode(-1, -1, -1, -1), 0
+    last_neighbor = starting_junction
+
+    while t < total_duration
+        best_neighbor, _ = get_best_neighbor!(path, last_neighbor, rg, current_idx; n=n)
+        current_idx += 1
+        path[current_idx] = best_neighbor.junction
+        last_neighbor = best_neighbor.junction
+        t += best_neighbor.duration
+    end
+
+    return path[1:current_idx]
+end

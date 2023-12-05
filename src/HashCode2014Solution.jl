9a87d88b48754fe39c7f89ef911b37abe63f7070
Adrian Anaemeje <anaemeje@mit.edu>
2023-12-05 18:02:00 -0500

    Add new functions and include files

--- src/HashCode2014Solution.jl
+++ src/HashCode2014Solution.jl
@@ -4,14 +4,26 @@ using HashCode2014
 using Random: AbstractRNG, default_rng
 
 export random_walk_modified
-export RouteGridNode, RouteGrid, create_grid, get_neighboring_junctions
+export RouteGridNode,
+    RouteGrid,
+    create_grid,
+    get_neighboring_junctions,
+    path_length,
+    greedy_path,
+    get_best_neighbor!,
+    get_best_neighbor
 export ComputeUpperBound
 
 include("random_walk_modified.jl")
 include("route_grid.jl")
+include("greedy_path.jl")
 include("ComputeUpperBound.jl")
+include("utils.jl")
 
 city = HashCode2014.read_city()
+solution = random_walk_modified(city)
+HashCode2014.is_feasible(solution, city)
+HashCode2014.total_distance(solution, city)
 
 rg = create_grid(city)
 

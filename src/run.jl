c4710a39c351870a04cbb518ade2f99e6e4c5d79
Adrian Anaemeje <anaemeje@mit.edu>
2023-12-06 15:46:30 -0500

    Fix formatting

--- src/run.jl
+++ src/run.jl
@@ -2,10 +2,15 @@ using BenchmarkTools
 
 city = HashCode2014Solution.read_city()
 
-rg = HashCode2014Solution.create_grid(city)
+rg = create_grid(city, 6)
 
 # Upper bounds
-(UB_distance_by_car, UB_duration_by_car) = HashCode2014Solution.ComputeUpperBound(city, city.total_duration, rg)
+(UB_distance_by_car, UB_duration_by_car) = HashCode2014Solution.ComputeUpperBound(
+    city, city.total_duration, rg
+)
+(UB_distance_by_car, UB_duration_by_car) = HashCode2014Solution.ComputeUpperBound(
+    city, city.total_duration, rg
+)
 
 UB_distance = sum(UB_distance_by_car)
 UB_duration, _ = findmax(UB_duration_by_car)
@@ -18,7 +23,14 @@ solution_rg = HashCode2014Solution.unique_random_walk(city, rg)
 # Run multiple simulations - select itinerary corresponding with furthest distance
 rw_distances = zeros(Float64, 15)
 @btime Threads.@threads for i in eachindex(rw_distances)
-    rw_distances[i] = HashCode2014Solution.total_distance(HashCode2014Solution.unique_random_walk(city, rg), city)
+    rw_distances[i] = HashCode2014Solution.total_distance(
+        HashCode2014Solution.unique_random_walk(city, rg), city
+    )
 end
 @show mean(rw_distances)
 @show findmax(rw_distances)
+
+## ---Greedy Path---
+path = greedy_path(rg, city.starting_junction, city.total_duration ÷ 4)
+@benchmark greedy_path($rg, $city.starting_junction, $city.total_duration ÷ 4)
+@benchmark path_length(path, rg)
